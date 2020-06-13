21eb73e42f5d2b6fbb900756f879d732dc8dda09
Yufenyuy Veyeh Didier <yveyeh@gmail.com>
2020-06-13 01:49:54 +0100

    Major updates.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,33 @@
 
-function outputMonthDays(month, year) {
-    var days = "";
+function computeInputs(month, year) {
+    var days = ""
     if (month >= 1 && month <= 12) {
         if (month == 4 || month == 6 || month == 9 || month == 11) {
-            days = "30 days";
+            days = "30 days"
         } else if (month == 2) {
             if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-                days = "29 days";
+                days = "29 days"
             } else {
-                days = "28 days";
+                days = "28 days"
             }
         } else {
-            days = "31 days";
+            days = "31 days"
         }
-        alert(days);
+        alert(days)
     } else {
-        alert("Please, enter a valid month number.");
+        alert("Please, enter a valid month and year.")
     }        
+}
+
+function getInputs() {
+    var month = document.getElementById('month').value
+    var year = document.getElementById('year').value
+    return [ month, year ]
+}
+
+function outputResults() {
+    var inputs = getInputs()
+    computeInputs(inputs[0], inputs[1])
 }
 
 
